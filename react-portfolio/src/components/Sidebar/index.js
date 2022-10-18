c92fff1672c42fe093643f02cb82ff8ba9b4b864
Victoria Le <leqvictoria@gmail.com>
2022-10-18 17:17:28 -0400

    adding logoV

--- react-portfolio/src/components/Sidebar/index.js
+++ react-portfolio/src/components/Sidebar/index.js
@@ -1,7 +1,15 @@
 import './index.scss'
+import {Link} from 'react-router-dom'
+import LogoV from '../../assets/images/logo-v.png'
 
 const Sidebar = () => {
-  return <>This is sidebar component</>
+  return (
+    <div className ='nav-bar'>
+        <Link className='logo' to='/'>
+            <img src={LogoV} alt='logo' />
+        </Link>
+    </div>
+  )
 }
 
 export default Sidebar 
