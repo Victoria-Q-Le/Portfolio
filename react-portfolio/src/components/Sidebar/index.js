f6be1be63db3e9f7fe3319abd86d6562affca2fb
Victoria Le <leqvictoria@gmail.com>
2022-10-18 17:54:06 -0400

    icons packages installed

--- react-portfolio/src/components/Sidebar/index.js
+++ react-portfolio/src/components/Sidebar/index.js
@@ -1,13 +1,25 @@
 import './index.scss'
-import {Link} from 'react-router-dom'
+import {Link, NavLink} from 'react-router-dom'
 import LogoV from '../../assets/images/logo-v.png'
+import LogoSubtitle from '../../assets/images/subtitle.png'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faHome} from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => {
   return (
     <div className ='nav-bar'>
         <Link className='logo' to='/'>
             <img src={LogoV} alt='logo' />
+            <img className='sub-logo' src={LogoSubtitle} alt='subtitle' />
         </Link>
+            {/* END OF LOGO AND SUBTITLE */}
+            {/* OTHER LINKS */}
+        <nav>
+            <NavLink exact='true' activeclassname='active' to='/'>
+                <FontAwesomeIcon icon={faHome} color='#4d4d4e' />
+            </NavLink>
+        </nav>
+
     </div>
   )
 }
