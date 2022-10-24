b934e419a108781286c7f05c2705b68a9f3ef69f
Victoria Le <leqvictoria@gmail.com>
2022-10-24 17:13:20 -0400

    animation tied to name and job arr

--- react-portfolio/src/pages/Home/index.js
+++ react-portfolio/src/pages/Home/index.js
@@ -14,13 +14,15 @@ const Home = () => {
         <div className='container home-page'>
             <div className='text-zone'>
                 <h1>
-                    Hi, 
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass} _12`}>i</span>
                     <br /> 
-                    I'm 
+                    <span className={`${letterClass} _13`}>I</span> 
+                    <span className={`${letterClass} _14`}>'m</span> 
                     <img src={LogoTitle} alt='developer' />
                     <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15}/>
                     <br />
-                    web developer
+                    <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={22}/>
                 </h1>
                 <h2>Full Stack Developer | Life Long Learner</h2>
                 <Link to='/contact' className='flat-button'>CONTACT ME</Link>
