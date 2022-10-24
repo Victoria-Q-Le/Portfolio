a1824ae3f0659fbaa42b0e2ceb81d56713cf63cb
Victoria Le <leqvictoria@gmail.com>
2022-10-24 15:45:58 -0400

    Working on the animation eff comp for letters

---
+++ react-portfolio/src/components/AnimatedLetters/index.js
@@ -0,0 +1,11 @@
+import './index.scss'
+
+const AnimatedLetters = ({letterClass, strArray,idx }) => { //3 args, the class thay will the applied to the letter, the letter array and the index/where it starts
+  return (
+    <span>
+        {strArray.map((char, i) => (
+            
+        ))}
+    </span>
+  )
+}
